1e62c828f5a223fc82d09258583e8fd1c1578107
eulalio <eulalio_cruz_nava@hotmail.com>
2020-07-23 17:49:05 -0500

    agregando nuevas ventanas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { MainComponent } from './componentes/main/main.component';
+import { PublicidadComponent } from './componentes/publicidad/publicidad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    PublicidadComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/publicidad/publicidad.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publicidad',
+  templateUrl: './publicidad.component.html'
+})
+export class PublicidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
